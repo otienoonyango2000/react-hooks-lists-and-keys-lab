b83c10d1185f85f3d0ac2e15231d8cc1f34171fe
otienoonyango2000 <otienoonyango2000@gmail.com>
2023-01-24 18:59:42 +0300

    list $ keys copleted

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, {useState}from "react";
 
 function ProjectItem({ name, about, technologies }) {
+
+  
+  const technologiesSpan= technologies.map((obj)=>{
+      return <span key={obj}>{obj}</span>
+    
+  })
+  
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {technologiesSpan}
+        
       </div>
     </div>
   );
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,15 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+  const myprojects = projects.map((obj)=>{
+    return <ProjectItem name={obj.name} key={obj.id} about={obj.about} technologies={obj.technologies}/>
+  }  
+  )
+  
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{myprojects}</div>
     </div>
   );
 }
